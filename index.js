4caaf4a5d77648409836536bf9facb96c1f9edbd
FrankKamala <frankkamala@gmail.com>
2022-05-25 18:34:10 +0300

    post

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/post/:id', async(req, res) => {
  // res.sendFile(path.resolve(__dirname,'public/post.html'))
  const post = await BlogPost.findById(req.params.id)
  res.render('post',{
-  post
+  post:post
   })
 //console.log(post)
 
