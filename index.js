fdb327385e6d4a6cf9eee4502514d1aaf7a67264
FrankKamala <frankkamala@gmail.com>
2022-05-25 16:24:53 +0300

    add posts find()

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express')
 const path = require('path')
 const ejs = require('ejs')
-const mongoose = require('mongoose')
+
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost')
+
+mongoose.connect('mongodb://localhost/blog_db', {useNewUrlParser: true})
+
 
 const app = express()
 const port = 3000
-mongoose.connect('mongodb://localhost/blog_db', {useNewUrlParser: true})
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.set('view engine','ejs')
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async(req, res) => {
+  const blogposts = await BlogPost.find()
+  res.render('index',
+  {blogposts:blogposts}
+  //or {blogposts}
+  )
 })
 app.get('/about', (req, res) => {
   res.render('about')
@@ -33,10 +42,22 @@ app.get('/posts/new', (req, res) => {
 // app.post('/home',  (req, res) =>{
 //   res.send('Jungle!')
 // })
-app.post('/posts/store',(req,res)=>{
-  console.log(req.body)
+
+//saveData
+// app.post('/posts/store',(req,res)=>{
+//   // model creates a new doc with browser data
+//   BlogPost.create(req.body,(error,blogpost)=>{
+//     console.log(req.body)
+//     res.redirect('/')
+//   }
+//   )
+//   }) drop callback hells
+app.post('/posts/store', async (req,res)=>{
+  await BlogPost.create(req.body)
   res.redirect('/')
-})
+  })
+
+
 
 
 app.listen(port, () => {
