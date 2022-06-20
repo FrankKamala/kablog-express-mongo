fbb4b931c2e8c05ecdd0a2bba6833fd76e898faa
FrankKamala <frankkamala@gmail.com>
2022-06-20 18:37:37 +0300

    fix links and blog item err

--- controllers/getSinglePost.js
+++ controllers/getSinglePost.js
@@ -1,8 +1,11 @@
 const BlogPost = require('../models/BlogPost.js')
-module.exports=async(req,res)=>{
-    const blogpost = await BlogPost.findById(req.params.id)
-console.log(blogpost)
-res.render('post',{
-blogpost
-});
+module.exports=async(req,res)=>{ // res.sendFile(path.resolve(__dirname,'public/post.html'))
+    const post = await BlogPost.findById(req.params.id)
+    res.render('post',{
+     post:post
+     })
+   //console.log(post)
+   
+   
+    res.render('post')
 }
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 const ejs = require('ejs')
 const fileUpload = require('express-fileupload')
 const validateMiddleWare = (req,res,next)=>{
