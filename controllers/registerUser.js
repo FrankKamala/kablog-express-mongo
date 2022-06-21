b4829569ca522c822943be1dfaaf21a5a816e914
FrankKamala <frankkamala@gmail.com>
2022-06-22 01:47:47 +0300

    regis

--- controllers/registerUser.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/User')
-const path = reqiure('path')
-
-module.exports = (req,res)=>{
-    User.create(re.body,(error,userMjamo)=>{
-        res.redirect('/')
-    })
-    
-}
