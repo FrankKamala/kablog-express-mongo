e0ad99ea1b1a8737709e8e8c63ba17cb9b358833
FrankKamala <frankkamala@gmail.com>
2022-06-22 21:53:35 +0300

    password hash

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt');
+const { hash } = require('bcrypt');
 
 const UserSchema =  new Schema({
  first_name:String,
@@ -10,7 +12,16 @@ const UserSchema =  new Schema({
 
 
 
+
 });
+UserSchema.pre('save',function(next){
+    const user = this;
+    bcrypt.hash(user.password,10,(error,hash)=>{
+        user.password=hash
+        next()
+    })
+     
+})
  //achia model
  const User =mongoose.model('User',UserSchema);
  module.exports = User;
