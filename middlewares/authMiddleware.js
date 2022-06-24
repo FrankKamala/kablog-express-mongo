3eda5d159b49fdfd8f2bdaf85f99c67232622f10
FrankKamala <frankkamala@gmail.com>
2022-06-24 05:01:59 +0300

    authMiddleware  check user then proceed to Next() bg

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const User = require('../models/User')
 
 module.exports=(req,res,next)=>{
-    User.findById(req.session.UserId,(error,user)=>{
+    User.findById(req.session.userId,(error,user)=>{
 
         if(error||!user){
             return res.redirect('/')
